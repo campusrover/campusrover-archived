053e974140f0fcc7e92cc179872f02fa0a5ec575
Pito Salas <pitosalas@gmail.com>
2018-06-29 15:52:09 -0400

    Checkpoint work to git

--- cg_config.rb
+++ cg_config.rb
@@ -1,4 +1,4 @@
-require 'coursegen'
+require "coursegen"
 
 # Copyright string
 COPYRIGHT_STRING = "Copyright (2013-2018) R. Pito Salas, pitosalas@brandeis.edu"
@@ -9,28 +9,30 @@ AWS_BUCKET = "campusrover.org"
 # Course short name
 COURSE_SHORT_NAME = "Cosi119a"
 COURSE_LONG_NAME = "Autonomous Robotics Lab"
-COURSE_ABBREV = "CS119a"
+COURSE_ABBREV = "119a"
 
-SPRING_2018 = ScheduleDef.new(
-  first_day: "jan-15-2018",
-  weekdays: [:monday],
-  start_times: ["09:00"],
-  end_times: ["11:50"],
+SCHED = ScheduleDef.new(
+  first_day: "aug-29-2018",
+  weekdays: [:wednesday, :wednesday],
+  start_times: ["14:00", "18:30"],
+  end_times: ["16:50", "21:20"],
   start_time: "09:00",
   end_time: "11:50",
-  skips: ["feb-19-2018", "apr-2-2018"],
-  number: 15)
+  skips: ["sep-19-2018", "sep-19-2018"],
+  number: 15,
+)
 
 # Sections in the right hand margin of the page
 SECTION_CONFIG = [
   SectionDef.new("Intro", "intro", hidden: false, type: :section),
-  SectionDef.new("Spring 2018", "s2018spring", type: :lecture, schedule: SPRING_2018, bullet: :dash),
+  SectionDef.new("Schedule", "s2018fall", type: :lecture, schedule: SCHED, bullet: :dash),
   SectionDef.new("Fundamentals", "fundamentals", hidden: true, type: :section),
   SectionDef.new("Projects", "projects", hidden: false, type: :section),
   SectionDef.new("Background", "background", hidden: false, type: :section),
   SectionDef.new("Topics", "topics", hidden: true, type: :section),
   SectionDef.new("Extras", "extras", hidden: true, type: :section),
-  SectionDef.new("Fall 2017", "s2017fall", hidden: true, type: :section)
+  SectionDef.new("Fall 2017", "s2017fall", hidden: true, type: :section),
+  SectionDef.new("Spring 2018", "s2018spring", type: :lecture, schedule: SCHED, bullet: :dash),
 ]
 # Options are:
 # bullet_style: css style for each bullet
