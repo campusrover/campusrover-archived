adc8a31e71734a154538c0f483979bcc56bf3efb
Pito Salas <pitosalas@gmail.com>
2019-04-30 10:53:41 -0400

    small

--- cg_config.rb
+++ cg_config.rb
@@ -1,15 +1,15 @@
 require "coursegen"
 
 # Copyright string
-COPYRIGHT_STRING = "Copyright (2013-2019) R. Pito Salas, pitosalas@brandeis.edu"
+COPYRIGHT_STRING = "Copyright (2013-2019) R. Pito Salas, pitosalas@brandeis.edu".freeze
 
 # bucket for AWS Deployment of the course
-AWS_BUCKET = "campusrover.org"
+AWS_BUCKET = "campusrover.org".freeze
 
 # Course short name
-COURSE_SHORT_NAME = ""
-COURSE_LONG_NAME = "Autonomous Robotics Lab"
-COURSE_ABBREV = ""
+COURSE_SHORT_NAME = "ARL".freeze
+COURSE_LONG_NAME = "Autonomous Robotics Lab".freeze
+COURSE_ABBREV = "ARL".freeze
 
 SCHED = ScheduleDef.new(
   first_day: "jan-15-2019",
@@ -29,10 +29,10 @@ SECTION_CONFIG = [
   SectionDef.new("Talks", "rosbook", hidden: false, type: :section),
   SectionDef.new("Background", "background", hidden: false, type: :section),
   SectionDef.new("Fundamentals", "fundamentals", hidden: true, type: :section),
-  SectionDef.new("Topics", "topics", hidden: true, type: :section),
-]
+  SectionDef.new("Topics", "topics", hidden: true, type: :section)
+].freeze
 # Options are:
 # bullet_style: css style for each bullet
-STYLING_CONFIG = {bullet_style: "\"font-size: 60%; width: 10px; color: grey\""}
-HOME_PAGE = "/content/intro/01_syllabus.md.erb"
+STYLING_CONFIG = {bullet_style: "\"font-size: 60%; width: 10px; color: grey\""}.freeze
+HOME_PAGE = "/content/intro/01_syllabus.md.erb".freeze
 HELPFUL_BOX = false
